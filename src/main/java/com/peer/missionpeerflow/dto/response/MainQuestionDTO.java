cfb9cb24459afa9be9c90714c6d0dc0dae149868
Two-Jay <djeeee1272@gmail.com>
2023-07-11 05:17:19 +0900

    feat : A_MAI response에 필요한 DTO와 Mapper 구성 정의

--- src/main/java/com/peer/missionpeerflow/dto/response/MainQuestionDTO.java
+++ src/main/java/com/peer/missionpeerflow/dto/response/MainQuestionDTO.java
@@ -1,2 +1,21 @@
-package com.peer.missionpeerflow.dto.response;public class MainQuestionDTO {
+package com.peer.missionpeerflow.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+// A_MAI_00 에서 패스워드 미공개를 위한 DTO
+@AllArgsConstructor
+@Builder
+@Getter
+public class MainQuestionDTO {
+    private Long id;
+    private String nickname;
+    private String title;
+    private String category;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
