c401020c3b3cc4158aa97153bd6320f87511a676
Two-Jay <djeeee1272@gmail.com>
2023-07-11 02:28:28 +0900

    feat: sort 키워드 체크하여 분기화하는 로직 구현

--- src/main/java/com/peer/missionpeerflow/service/MainService.java
+++ src/main/java/com/peer/missionpeerflow/service/MainService.java
@@ -1,2 +1,40 @@
-package com.peer.missionpeerflow.service;public class MainService {
+package com.peer.missionpeerflow.service;
+
+import com.peer.missionpeerflow.entity.Question;
+import com.peer.missionpeerflow.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import com.peer.missionpeerflow.repository.MainRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MainService{
+    private final MainRepository mainRepository;
+
+    public Page<Question> getMainList(String category, String sort, int page, int size) {
+        try {
+            Sort sortClass = this.getSortClass(sort);
+            PageRequest pageRequest = PageRequest.of(page, size, Sort.by(sort));
+            return mainRepository.findAllByCategory(category, pageRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new NotFoundException("Not Found : getMainList");
+        }
+    }
+
+    private Sort getSortClass(String sortKeyword) {
+        switch (sortKeyword) {
+            case "latest":
+                return Sort.by("createdAt").descending();
+            case "views":
+                return Sort.by("views").descending();
+            case "recommend":
+                return Sort.by("recommend").descending();
+            default:
+                return Sort.by("id").descending();
+        }
+    }
 }
