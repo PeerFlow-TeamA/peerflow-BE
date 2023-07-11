49555b9b6fc23e88c7d202d7f9783377bcc86893
Two-Jay <djeeee1272@gmail.com>
2023-07-11 19:08:11 +0900

    fix : ResponseEntity를 이용해서 4xx 5xx 응답 구현

--- src/main/java/com/peer/missionpeerflow/controller/MainController.java
+++ src/main/java/com/peer/missionpeerflow/controller/MainController.java
@@ -4,6 +4,7 @@ import com.peer.missionpeerflow.dto.response.MainQuestionDTO;
 import com.peer.missionpeerflow.entity.Question;
 import com.peer.missionpeerflow.service.MainService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.QueryParameterException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.peer.missionpeerflow.exception.NotFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,12 +29,16 @@ public class MainController {
                                                             @PathVariable int page,
                                                             @PathVariable int size) {
         try {
+            if (category == null || sort == null || page < 0 || size < 0) {
+                throw new QueryParameterException("Query Parameter Error : getMainList");
+            }
             Page<MainQuestionDTO> questionDTOList = this.mainService.getMainList(category, sort, page, size);
             model.addAttribute("questionList", questionDTOList);
             return ResponseEntity.status(HttpStatus.OK).body(questionDTOList);
-        } catch (NotFoundException e) {
-            e.printStackTrace();
-            return null;
+        } catch (QueryParameterException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 }
