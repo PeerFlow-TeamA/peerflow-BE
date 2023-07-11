c6ac7579e25e8caf6ab4e28e7e46d24f662bc8ea
Two-Jay <djeeee1272@gmail.com>
2023-07-11 18:44:11 +0900

    feat : 컨트롤러에서 응답시 ResponseEntity 활용하도록 변경

--- src/main/java/com/peer/missionpeerflow/controller/MainController.java
+++ src/main/java/com/peer/missionpeerflow/controller/MainController.java
@@ -5,6 +5,8 @@ import com.peer.missionpeerflow.entity.Question;
 import com.peer.missionpeerflow.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,15 +19,17 @@ public class MainController {
     private final MainService mainService;
 
     @GetMapping("/v1?category={category}&sort={sort}&page={page}&size={size}")
-    public Page<MainQuestionDTO> getMainList(Model model,
-                                                      @PathVariable String category,
-                                                      @PathVariable String sort,
-                                                      @PathVariable int page,
-                                                      @PathVariable int size) {
+    public ResponseEntity<Page<MainQuestionDTO>> getMainList(Model model,
+                                                            @PathVariable String category,
+                                                            @PathVariable String sort,
+                                                            @PathVariable int page,
+                                                            @PathVariable int size) {
         try {
             Page<MainQuestionDTO> questionDTOList = this.mainService.getMainList(category, sort, page, size);
             model.addAttribute("questionList", questionDTOList);
-            return questionDTOList;
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(questionDTOList);
         } catch (NotFoundException e) {
             e.printStackTrace();
             return null;
