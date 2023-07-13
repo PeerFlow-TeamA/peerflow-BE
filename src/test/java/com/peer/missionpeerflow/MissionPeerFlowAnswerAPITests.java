d4b757c6d61c87806ba39961ff345f96f6bb5794
Two-Jay <djeeee1272@gmail.com>
2023-07-13 18:26:22 +0900

    test : C_DET_02 테스트 추가

--- src/test/java/com/peer/missionpeerflow/MissionPeerFlowAnswerAPITests.java
+++ src/test/java/com/peer/missionpeerflow/MissionPeerFlowAnswerAPITests.java
@@ -24,7 +24,7 @@ public class MissionPeerFlowAnswerAPITests {
     private RequestBodyJSONGenerator requestBodyJsonGenerator = new RequestBodyJSONGenerator();
 
     @Test
-    void C_DET_01_service_green_00() throws Exception {
+    void mockMVCtest_C_DET_01_GREEN_00() throws Exception {
 
         String question_requestBody = requestBodyJsonGenerator
                 .addAttribute("title", "postman so hard")
@@ -63,7 +63,7 @@ public class MissionPeerFlowAnswerAPITests {
     }
 
     @Test
-    void C_DET_01_service_RED_00() throws Exception {
+    void mockMVCtest_C_DET_01_RED_00() throws Exception {
         String answer_requestBody = requestBodyJsonGenerator
                 .addAttribute("questionId", "1")
                 .addAttribute("nickname", "jun")
@@ -80,5 +80,54 @@ public class MissionPeerFlowAnswerAPITests {
 
         assertTrue(resultAnswer.getResponse().getStatus() == HttpStatus.NOT_FOUND.value());
         assertTrue(resultAnswer.getResponse().getContentAsString().equals("{\"message\":\"Question not found\"}"));
+    }
+
+    @Test
+    void mockMVCtest_C_DET_02_GREEN_00() throws Exception {
+        String question_requestBody = requestBodyJsonGenerator
+                .addAttribute("title", "postman so hard")
+                .addAttribute("nickname", "jun")
+                .addAttribute("password", "1234")
+                .addAttribute("category", "minishell")
+                .addAttribute("content", "postman so hard")
+                .addAttribute("createdAt", "2021-06-01/00:00:00")
+                .toJson();
+
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.post("/v1/question")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(question_requestBody)
+        ).andReturn();
+
+        String answer_requestBody = requestBodyJsonGenerator
+                .addAttribute("questionId", "1")
+                .addAttribute("nickname", "jun")
+                .addAttribute("password", "1234")
+                .addAttribute("content", "postman so hard")
+                .addAttribute("createdAt", "2021-06-01/00:00:00")
+                .toJson();
+
+        MvcResult resultAnswer = mockMvc.perform(
+                MockMvcRequestBuilders.post("/v1/answer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(answer_requestBody)
+        ).andReturn();
+
+        String answer_requestBody2 = requestBodyJsonGenerator
+                .addAttribute("questionId", "1")
+                .addAttribute("nickname", "jun")
+                .addAttribute("password", "1234")
+                .addAttribute("content", "peerflow is soooo easy")
+                .addAttribute("createdAt", "2021-06-01/00:00:00")
+                .toJson();
+
+        MvcResult resultAnswer2 = mockMvc.perform(
+                MockMvcRequestBuilders.put("/v1/answer/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(answer_requestBody2)
+        ).andReturn();
+
+        assertTrue(resultAnswer2.getResponse().getStatus() == HttpStatus.CREATED.value());
+        assertTrue(resultAnswer2.getResponse().getContentAsString().equals("{\"message\":\"Answer modified successfully\"}"));
     }
 }
