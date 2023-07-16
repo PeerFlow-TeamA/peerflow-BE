187dd82d977dbf8761068a78b14935769266bc47
Two-Jay <djeeee1272@gmail.com>
2023-07-16 17:10:49 +0900

    MainController fix

--- src/main/java/com/peer/missionpeerflow/repository/MainRepository.java
+++ src/main/java/com/peer/missionpeerflow/repository/MainRepository.java
@@ -1,5 +1,6 @@
 package com.peer.missionpeerflow.repository;
 
+import com.peer.missionpeerflow.util.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import com.peer.missionpeerflow.entity.Question;
@@ -7,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 
 
 public interface MainRepository extends PagingAndSortingRepository<Question, Long> {
-    Page<Question> findAllByCategory(String category, Pageable pageable);
+    Page<Question> findAllByCategory(Category category, Pageable pageable);
 
     Page<Question> findAllByTitleContaining(String title, Pageable pageable);
     Page<Question> findAll(Pageable pageable);
